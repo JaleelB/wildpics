69ebd5360c40c733a178bb09afa80c7538f2994d
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-07-11 22:23:24 -0400

    chore: added larger screens breakpoint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2xl": "1600px",
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
