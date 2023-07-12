3d3bde161260ddce119082ceb0bef1f5bbb8139e
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-07-11 22:35:03 -0400

    feat: homepage + image gallery

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,17 +2,19 @@ import Link from "next/link";
 
 export default function Header(){
     return(
-        <header className="w-screen z-40 px-4">
-            <div className="container max-w-[1400px] flex h-20 items-center justify-between py-6 mx-auto">
+        <header className="w-screen z-40 px-4 text-white">
+            <div className="flex h-16 md:h-20 items-center justify-between px-5 sm:px-10 text-sm md:text-base">
                 <Link href="/">
                     wildpics
                 </Link>
-                <nav className="flex gap-8">
-                    <Link href="/">
-                        about
+                <nav className="flex gap-4 sm:gap-8">
+                    <Link href="/" className="relative group">
+                        <span>about</span>
+                        <span className="absolute left-0 -bottom-1 w-0 h-[1px] bg-black z-10 group-hover:w-full group-hover:transition-all"></span>
                     </Link>
-                    <Link href="https://github.com/JaleelB/wildpics">
-                        github
+                    <Link href="https://github.com/JaleelB/wildpics" className="relative group">
+                        <span>github</span>
+                        <span className="absolute left-0 -bottom-1 w-0 h-[1px] bg-black z-10 group-hover:w-full group-hover:transition-all"></span>
                     </Link>
                 </nav>
             </div>
