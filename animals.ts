d535cd233d2a95ae9795f88f416c579a034151e1
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-07-11 22:02:13 -0400

    feat: animal list

---
+++ animals.ts
@@ -0,0 +1,116 @@
+export const animals = [
+    {
+      id: 1,
+      name: "Lion",
+      imageSrc:
+        "https://images.pexels.com/photos/12004890/pexels-photo-12004890.jpeg",
+    },
+    {
+      id: 2,
+      name: "Jaguar",
+      imageSrc:
+        "https://images.pexels.com/photos/235674/pexels-photo-235674.jpeg",
+    },
+    {
+      id: 3,
+      name: "Wolf",
+      imageSrc:
+        "https://images.pexels.com/photos/3749012/pexels-photo-3749012.jpeg",
+    },
+    {
+      id: 4,
+      name: "Buffalo",
+      imageSrc:
+        "https://images.pexels.com/photos/16116694/pexels-photo-16116694/free-photo-of-african-buffalo-lying-in-the-grass.jpeg",
+    },
+    {
+      id: 5,
+      name: "Crocodile",
+      imageSrc:
+        "https://images.pexels.com/photos/7081500/pexels-photo-7081500.jpeg",
+    },
+    {
+      id: 6,
+      name: "Green Snake",
+      imageSrc:
+        "https://images.pexels.com/photos/2694056/pexels-photo-2694056.jpeg",
+    },
+    {
+      id: 7,
+      name: "Chameleon",
+      imageSrc:
+        "https://images.pexels.com/photos/1887726/pexels-photo-1887726.jpeg",
+    },
+    {
+      id: 9,
+      name: "Antelope",
+      imageSrc:
+        "https://images.pexels.com/photos/15247550/pexels-photo-15247550/free-photo-of-standing-antelope-on-meadow.jpeg",
+    },
+    {
+        id: 10,
+        name: "Meerkat",
+        imageSrc:
+          "https://images.pexels.com/photos/15535804/pexels-photo-15535804/free-photo-of-meerkat-standing-and-looking.jpeg",
+      },
+      {
+        id: 11,
+        name: "Hedgehog",
+        imageSrc:
+          "https://images.pexels.com/photos/206862/pexels-photo-206862.jpeg",
+      },
+      {
+        id: 12,
+        name: "Elephant",
+        imageSrc:
+          "https://images.pexels.com/photos/11760799/pexels-photo-11760799.jpeg",
+      },
+      {
+        id: 13,
+        name: "Jellyfish",
+        imageSrc:
+          "https://images.pexels.com/photos/2602451/pexels-photo-2602451.jpeg",
+      },
+      {
+        id: 14,
+        name: "Kangaroo",
+        imageSrc:
+          "https://images.pexels.com/photos/13166855/pexels-photo-13166855.jpeg",
+      },
+      {
+        id: 15,
+        name: "Koaala",
+        imageSrc:
+          "https://images.pexels.com/photos/6090284/pexels-photo-6090284.jpeg",
+      },
+      {
+        id: 16,
+        name: "Shark",
+        imageSrc:
+          "https://images.pexels.com/photos/16916029/pexels-photo-16916029/free-photo-of-photo-of-a-shark-in-the-water.jpeg",
+      },
+      {
+        id: 17,
+        name: "Gorilla",
+        imageSrc:
+          "https://images.pexels.com/photos/13044256/pexels-photo-13044256.jpeg",
+      },
+      {
+        id: 18,
+        name: "Sea Turtle",
+        imageSrc:
+          "https://images.pexels.com/photos/2765872/pexels-photo-2765872.jpeg",
+      },
+      {
+        id: 19,
+        name: "Penguin",
+        imageSrc:
+          "https://images.pexels.com/photos/4169869/pexels-photo-4169869.jpeg",
+      },
+      {
+        id: 20,
+        name: "Frog",
+        imageSrc:
+          "https://images.pexels.com/photos/12159464/pexels-photo-12159464.jpeg",
+      },
+];
