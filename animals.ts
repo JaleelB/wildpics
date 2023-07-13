56b345a7568b125ba33e1d1d35111f5425c96186
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-07-12 21:20:41 -0400

    chore: increased animal count

--- animals.ts
+++ animals.ts
@@ -1,116 +1,152 @@
-export const animals = [
-    {
-      id: "lion",
-      name: "Lion",
+export type Animal = {
+  id: string
+  name: string
+  imageSrc: string
+}
+
+export const animals:Animal[] = [
+  {
+    id: "lion",
+    name: "Lion",
+    imageSrc:
+      "https://images.pexels.com/photos/12004890/pexels-photo-12004890.jpeg",
+  },
+  {
+    id: "jaguar",
+    name: "Jaguar",
+    imageSrc:
+      "https://images.pexels.com/photos/235674/pexels-photo-235674.jpeg",
+  },
+  {
+    id: "wolf",
+    name: "Wolf",
+    imageSrc:
+      "https://images.pexels.com/photos/3749012/pexels-photo-3749012.jpeg",
+  },
+  {
+    id: "buffalo",
+    name: "Buffalo",
+    imageSrc:
+      "https://images.pexels.com/photos/16116694/pexels-photo-16116694/free-photo-of-african-buffalo-lying-in-the-grass.jpeg",
+  },
+  {
+    id: "crocodile",
+    name: "Crocodile",
+    imageSrc:
+      "https://images.pexels.com/photos/7081500/pexels-photo-7081500.jpeg",
+  },
+  {
+    id: "snake",
+    name: "Green Snake",
+    imageSrc:
+      "https://images.pexels.com/photos/2694056/pexels-photo-2694056.jpeg",
+  },
+  {
+    id: "chameleon",
+    name: "Chameleon",
+    imageSrc:
+      "https://images.pexels.com/photos/1887726/pexels-photo-1887726.jpeg",
+  },
+  {
+    id: "antelope",
+    name: "Antelope",
+    imageSrc:
+      "https://images.pexels.com/photos/15247550/pexels-photo-15247550/free-photo-of-standing-antelope-on-meadow.jpeg",
+  },
+  {
+      id: "meerkat",
+      name: "Meerkat",
       imageSrc:
-        "https://images.pexels.com/photos/12004890/pexels-photo-12004890.jpeg",
+        "https://images.pexels.com/photos/15535804/pexels-photo-15535804/free-photo-of-meerkat-standing-and-looking.jpeg",
     },
     {
-      id: "jaguar",
-      name: "Jaguar",
+      id: "hedgehog",
+      name: "Hedgehog",
       imageSrc:
-        "https://images.pexels.com/photos/235674/pexels-photo-235674.jpeg",
+        "https://images.pexels.com/photos/206862/pexels-photo-206862.jpeg",
     },
     {
-      id: "wolf",
-      name: "Wolf",
+      id: "elephant",
+      name: "Elephant",
       imageSrc:
-        "https://images.pexels.com/photos/3749012/pexels-photo-3749012.jpeg",
+        "https://images.pexels.com/photos/11760799/pexels-photo-11760799.jpeg",
     },
     {
-      id: "buffalo",
-      name: "Buffalo",
+      id: "jellyfish",
+      name: "Jellyfish",
       imageSrc:
-        "https://images.pexels.com/photos/16116694/pexels-photo-16116694/free-photo-of-african-buffalo-lying-in-the-grass.jpeg",
+        "https://images.pexels.com/photos/2602451/pexels-photo-2602451.jpeg",
     },
     {
-      id: "crocodile",
-      name: "Crocodile",
+      id: "kangaroo",
+      name: "Kangaroo",
       imageSrc:
-        "https://images.pexels.com/photos/7081500/pexels-photo-7081500.jpeg",
+        "https://images.pexels.com/photos/13166855/pexels-photo-13166855.jpeg",
     },
     {
-      id: "snake",
-      name: "Green Snake",
+      id: "koala",
+      name: "Koaala",
       imageSrc:
-        "https://images.pexels.com/photos/2694056/pexels-photo-2694056.jpeg",
+        "https://images.pexels.com/photos/6090284/pexels-photo-6090284.jpeg",
     },
     {
-      id: "chameleon",
-      name: "Chameleon",
+      id: "shark",
+      name: "Shark",
       imageSrc:
-        "https://images.pexels.com/photos/1887726/pexels-photo-1887726.jpeg",
+        "https://images.pexels.com/photos/16916029/pexels-photo-16916029/free-photo-of-photo-of-a-shark-in-the-water.jpeg",
     },
     {
-      id: "antelope",
-      name: "Antelope",
+      id: "gorilla",
+      name: "Gorilla",
       imageSrc:
-        "https://images.pexels.com/photos/15247550/pexels-photo-15247550/free-photo-of-standing-antelope-on-meadow.jpeg",
+        "https://images.pexels.com/photos/13044256/pexels-photo-13044256.jpeg",
     },
     {
-        id: "meerkat",
-        name: "Meerkat",
-        imageSrc:
-          "https://images.pexels.com/photos/15535804/pexels-photo-15535804/free-photo-of-meerkat-standing-and-looking.jpeg",
-      },
-      {
-        id: "hedgehog",
-        name: "Hedgehog",
-        imageSrc:
-          "https://images.pexels.com/photos/206862/pexels-photo-206862.jpeg",
-      },
-      {
-        id: "elephant",
-        name: "Elephant",
-        imageSrc:
-          "https://images.pexels.com/photos/11760799/pexels-photo-11760799.jpeg",
-      },
-      {
-        id: "jellyfish",
-        name: "Jellyfish",
-        imageSrc:
-          "https://images.pexels.com/photos/2602451/pexels-photo-2602451.jpeg",
-      },
-      {
-        id: "kangaroo",
-        name: "Kangaroo",
-        imageSrc:
-          "https://images.pexels.com/photos/13166855/pexels-photo-13166855.jpeg",
-      },
-      {
-        id: "koala",
-        name: "Koaala",
-        imageSrc:
-          "https://images.pexels.com/photos/6090284/pexels-photo-6090284.jpeg",
-      },
-      {
-        id: "shark",
-        name: "Shark",
-        imageSrc:
-          "https://images.pexels.com/photos/16916029/pexels-photo-16916029/free-photo-of-photo-of-a-shark-in-the-water.jpeg",
-      },
-      {
-        id: "gorilla",
-        name: "Gorilla",
-        imageSrc:
-          "https://images.pexels.com/photos/13044256/pexels-photo-13044256.jpeg",
-      },
-      {
-        id: "turtle",
-        name: "Sea Turtle",
-        imageSrc:
-          "https://images.pexels.com/photos/2765872/pexels-photo-2765872.jpeg",
-      },
-      {
-        id: "penguin",
-        name: "Penguin",
-        imageSrc:
-          "https://images.pexels.com/photos/4169869/pexels-photo-4169869.jpeg",
-      },
-      {
-        id: "frog",
-        name: "Frog",
-        imageSrc:
-          "https://images.pexels.com/photos/12159464/pexels-photo-12159464.jpeg",
-      },
+      id: "turtle",
+      name: "Sea Turtle",
+      imageSrc:
+        "https://images.pexels.com/photos/7082102/pexels-photo-7082102.jpeg",
+    },
+    {
+      id: "penguin",
+      name: "Penguin",
+      imageSrc:
+        "https://images.pexels.com/photos/4169869/pexels-photo-4169869.jpeg",
+    },
+    {
+      id: "frog",
+      name: "Frog",
+      imageSrc:
+        "https://images.pexels.com/photos/12159464/pexels-photo-12159464.jpeg",
+    },
+    {
+      id: "starfish",
+      name: "Starfish",
+      imageSrc:
+        "https://images.pexels.com/photos/10017673/pexels-photo-10017673.jpeg",
+    },
+    {
+      id: "octopus",
+      name: "Octopus",
+      imageSrc:
+        "https://images.pexels.com/photos/3046629/pexels-photo-3046629.jpeg",
+    },
+    {
+      id: "crab",
+      name: "Crab",
+      imageSrc:
+        "https://images.pexels.com/photos/584501/pexels-photo-584501.jpeg",
+    },
+    {
+      id: "mole",
+      name: "Mole",
+      imageSrc:
+        "https://images.pexels.com/photos/88512/mole-nature-animals-molehills-88512.jpeg",
+    },
+    {
+      id: "red-panda",
+      name: "Red Panda",
+      imageSrc:
+        "https://images.pexels.com/photos/146102/pexels-photo-146102.jpeg",
+    }
 ];
